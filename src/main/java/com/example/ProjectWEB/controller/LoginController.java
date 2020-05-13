825aefcbe45b8886d64fdb07280b5915d5b82220
Elmira <elmira.kurmanbekova@iaau.edu.kg>
2020-05-13 13:04:44 +0600

    Minor bug fixes

---
+++ src/main/java/com/example/ProjectWEB/controller/LoginController.java
@@ -0,0 +1,54 @@
+package com.example.ProjectWEB.controller;
+
+
+import com.example.ProjectWEB.entity.User;
+import com.example.ProjectWEB.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value={"/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String createNewUser(@Valid User user, BindingResult bindingResult, HttpServletRequest request) {
+        User userExists = userService.findUserByEmail(user.getEmail());
+        if (userExists != null) {
+            bindingResult
+                    .rejectValue("email", "error.user",
+                            "*There is already a user registered with the email provided");
+        }
+         else {
+                userService.saveUser(user);
+
+        }
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/ProjectWEB/controller/MainController.java
@@ -0,0 +1,60 @@
+package com.example.ProjectWEB.controller;
+
+import com.example.ProjectWEB.repository.ObjectRepository;
+import com.example.ProjectWEB.service.ObjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    ObjectService objectService;
+
+    @Autowired
+    ObjectRepository objectRepository;
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+//    @RequestMapping("/events")
+//    public String blog(){
+//        return "blog";
+//    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @RequestMapping("/lostForm")
+    public String lostForm(){
+        return "lostForm";
+    }
+
+    @RequestMapping("/objects")
+    public String found(Model model) {
+        model.addAttribute("objects", objectRepository.findByStatus("found"));
+        return "found";
+    }
+    @RequestMapping("/lostObjects")
+    public String lost(Model model) {
+        model.addAttribute("objects", objectRepository.findByStatus("lost"));
+        return "lost";
+    }
+
+}
